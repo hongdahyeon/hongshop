74888dba10e88da7fcef0a233e2fa250a92e6584
hongdahyeon <hyeon8287@gmail.com>
2023-06-27 09:04:06 +0900

    commit1 - settings

---
+++ src/main/java/hongshop/hongshop/HongshopApplication.java
@@ -0,0 +1,13 @@
+package hongshop.hongshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hongshop/hongshop/HongshopApplicationTests.java
@@ -0,0 +1,13 @@
+package hongshop.hongshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
