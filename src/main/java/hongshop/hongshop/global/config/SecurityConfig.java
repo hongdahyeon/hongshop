e9ea18d0b80aba00f87ea89ad260d3be800f2767
hongdahyeon <hyeon8287@gmail.com>
2023-07-14 15:44:33 +0900

    commit8 - filter chian autorize 코드 수정

--- src/main/java/hongshop/hongshop/global/config/SecurityConfig.java
+++ src/main/java/hongshop/hongshop/global/config/SecurityConfig.java
@@ -30,14 +30,15 @@ public class SecurityConfig  {
         http
                 .authorizeRequests(authorize ->                         // url 기반 보안을 구성하는 http 객체의 메서드 -> 다양한 유형의 사용자가 액세스할 수 있는 url 정의
                         authorize
-                                .antMatchers("/user/**").authenticated()                            // "/user/"로 시작하는 모든 url은 인증된 사용자만 액세스 가능
+                                .antMatchers("/login").permitAll()                                  // 로그인 페이지의 경우 모두 접근 가능
+                                .antMatchers("/user/**").access("hasRole('ROLE_USER')")     // "/user/"로 시작하는 모든 url은 "ROLE_USER"의 역할을 갖은 사용자만 액세스 가능
                                 .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")   // "/admin/"로 시작하는 모든 url은 "ROLE_ADMIN"의 역할을 갖은 사용자만 액세스 가능
                                 .anyRequest().permitAll()                                             // 앞선 패턴들을 제외한 다른 요청들은 모두에게 허용
                         )
                 .formLogin(formLoginConfigurer ->
                         formLoginConfigurer
-                                .usernameParameter("userId")            // 사용자 이름 필드에 사용되는 매개변수 이름의 기본값인 "username" 대신 "userId" 사용하도록 설정
                                 .loginPage("/login")                    // 로그인 페이지가 있는 url 지정
+                                .usernameParameter("userId")            // 사용자 이름 필드에 사용되는 매개변수 이름의 기본값인 "username" 대신 "userId" 사용하도록 설정
                                 .loginProcessingUrl("/loginProc")       // 처리를 위해 로그인 양식을 제출할 url을 지정
                                 .defaultSuccessUrl("/")                 // 로그인 성공 후, 리다렉션할 기본 url 설정
 
