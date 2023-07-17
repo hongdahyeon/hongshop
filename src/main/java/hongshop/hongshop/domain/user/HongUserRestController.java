6a6080b48a4b0fde35cc2b504cfa5aa25534b02e
hongdahyeon <hyeon8287@gmail.com>
2023-07-17 13:30:47 +0900

    commit14 - swagger annotation

--- src/main/java/hongshop/hongshop/domain/user/HongUserRestController.java
+++ src/main/java/hongshop/hongshop/domain/user/HongUserRestController.java
@@ -2,6 +2,7 @@ package hongshop.hongshop.domain.user;
 
 import hongshop.hongshop.global.auth.PrincipalDetails;
 import hongshop.hongshop.global.response.ApiDocumentResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -19,21 +20,22 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@Tag(name = "hong user controller", description = "유저 컨트롤러")
+@Tag(name = "hong user rest controller", description = "유저 Rest 컨트롤러")
 @RequiredArgsConstructor
 public class HongUserRestController {
 
     private final HongUserService hongUserService;
 
     @PostMapping("/user")
+    @Operation(summary = "insert user", description = "회원가입")
     @ApiDocumentResponse
     public ResponseEntity<Long> insert(@RequestBody HongUserDTO hongUserDTO){
-        System.out.println("hongUserDTO = " + hongUserDTO);
         Long userUid = hongUserService.joinUser(hongUserDTO);
         return ResponseEntity.ok(userUid);
     }
 
     @GetMapping("/user")
+    @Operation(summary = "get login user", description = "로그인한 회원 정보 가져오기")
     @ApiDocumentResponse
     public ResponseEntity<HongUser> loginUser(@AuthenticationPrincipal PrincipalDetails principalDetails){
         HongUser user = principalDetails.getUser();
@@ -41,6 +43,7 @@ public class HongUserRestController {
     }
 
     @GetMapping("/userById")
+    @Operation(summary = "get user info by userId", description = "회원 아이디로 정보 가져오기")
     @ApiDocumentResponse
     public ResponseEntity<HongUserVO> getUserById(String userId){
         HongUserVO hongUserByUserId = hongUserService.getHongUserByUserId(userId);
