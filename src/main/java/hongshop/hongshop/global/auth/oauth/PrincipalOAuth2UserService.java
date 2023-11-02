d044abbf0ddc12e70fde08e606fc5bc37c9a8aca
hongdahyeon <hyeon8287@gmail.com>
2023-11-02 10:14:55 +0900

    commit 84 : 네이버 로그인 구현

--- src/main/java/hongshop/hongshop/global/auth/oauth/PrincipalOAuth2UserService.java
+++ src/main/java/hongshop/hongshop/global/auth/oauth/PrincipalOAuth2UserService.java
@@ -6,7 +6,6 @@ import hongshop.hongshop.domain.user.HongUser;
 import hongshop.hongshop.global.auth.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -28,15 +27,26 @@ public class PrincipalOAuth2UserService extends DefaultOAuth2UserService {
         String provider = userRequest.getClientRegistration().getRegistrationId();
         Map<String, Object> userInfo = user.getAttributes();
 
-        Map<String, Object> kakaoAccount = user.getAttribute("kakao_account");
-        String userId = provider + "_" + userInfo.get("id");
-        String email = (String) kakaoAccount.get("email");
-        String name = (String) ((Map<String, Object>) kakaoAccount.get("profile")).get("nickname");
+        String userId = "";
+        String email = "";
+        String name = "";
 
-        log.info("provider : {} ", provider);           // kakao
-        log.info("userId : {} ", userId);               // kakao_3142662278
-        log.info("name : {} ", name);                   // 다현
-        log.info("email : {} ", email);                 // julie0427@kakao.com
+        switch (provider) {
+            case "kakao":
+                Map<String, Object> kakaoAccount = user.getAttribute("kakao_account");
+                userId = provider + "_" + userInfo.get("id");
+                email = (String) kakaoAccount.get("email");
+                name = (String) ((Map<String, Object>) kakaoAccount.get("profile")).get("nickname");
+                break;
+            case "naver":
+                Map<String, Object> naverAccount = user.getAttribute("response");
+                userId = provider + "_" + (String) naverAccount.get("id");
+                email = (String) naverAccount.get("email");
+                name = (String) naverAccount.get("name");
+                break;
+        }
+
+        log.info("provider: {} -> userId : {}, name : {}, email : {}", provider, userId, name, email);
 
         HongUser hongUser = hongSocialUserService.findUser(userId);
         if(hongUser == null) {
