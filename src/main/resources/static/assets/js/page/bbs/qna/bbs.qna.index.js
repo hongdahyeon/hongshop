df0cfa68fad7c90895e61bed862a4a1be4f23bfe
hongdahyeon <hyeon8287@gmail.com>
2023-12-06 15:20:51 +0900

    commit 154 : bbs 하위 html -> js와 분리

---
+++ src/main/resources/static/assets/js/page/bbs/qna/bbs.qna.index.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    table
+        .get(`/api/postsWithFileByPostType/${id}`)
+        .add(new Column("regId").title("").width("5%").center().formatter(function(cell) {
+            const cellData = cell.getValue()
+            const classNm = (cellData == user.id) ? 'front-medium-2' : 'front-medium-2 lock'
+            const feathers = (cellData == user.id) ? 'unlock' : 'lock'
+            return `<i class="${classNm}" data-feather=${feathers} />`
+        }))
+        .add(new Column("index").title("#").width("10%").center())
+        .add(new Column("title").title("제목").width("30%").left())
+        .add(new Column("readCnt").title("조회수").width("20%").center())
+        .add(new Column("file").title("첨부파일").width("20%").center().formatter(function(cell) {
+            const cellData = cell.getValue()
+            return (cellData === null) ? '0 개' : `${cellData['fileList'].length} 개`
+        }))
+        .add(new Column("answerList").title("답변").width("10%").formatter(function(cell){ return `${cell.getValue().length} 개` }))
+        .init()
+
+    table.rowClick((data, row) => {
+        if(user['role'] == 'ROLE_USER' && data['regId'] != user['id']) Util.alert("본인이 작성한 게시글에 대해서만 볼 수 있습니다.", 'w', 'w')
+        else window.location.href = `/bbs/view/${data['id']}`
+    })
+
+    table.afterComplete(() => {
+        feather.replace()
+    })
+
+    $("#new-bbsctt").on("click", function(e) {
+        window.location.href = `/bbs/save/${type['postTypeId']}`
+    })
+})
---
+++ src/main/resources/static/assets/js/page/bbs/qna/bbs.qna.save.js
@@ -0,0 +1,78 @@
+$(document).ready(function(){
+
+    $("#list-btn").on("click", function(e){
+        Util.confirm(`작성 정보가 저장되지 않을수도 있습니다. <br/> 목록으로 돌아가시겠습니까?`).then((isOk) => {
+            if(isOk) {
+                window.location.href = `/bbs/${type['postTypeId']}`
+            }
+        })
+    })
+
+    window.addEventListener("load", function() {
+        const form = document.getElementById("qna-form")
+        form.addEventListener("submit", function(event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                form.classList.add("was-validated")
+            } else {
+                event.preventDefault();
+                if(ckEditor.getData().length === 0) Util.alert("내용을 입력해주세요.", 'w', 'w')
+                else {
+                    const obj = {
+                        'title' : $("#title").val(),
+                        'content': ckEditor.getData(),
+                        'hongPostTypeId' : type['postTypeId'],
+                        'deleteFile': uploadFile.getDeleteFile()
+                    }
+
+                    if(uploadFile.getFileGroupId()) obj['fileGroupId'] = uploadFile.getFileGroupId()
+
+                    Http.post(`/api/post`, obj).then((res) => {
+                        if(res['httpStatus'] === 200){
+                            Util.alert("저장되었습니다.").then(() => {
+                                window.location.href = `/bbs/${type['postTypeId']}`
+                            })
+                        }
+                    })
+                }
+            }
+        })
+    })
+
+    $('#btn-upload').on("click", function (e) {
+        e.preventDefault();
+        $('#fileUpload').click();
+    });
+
+    $("#fileUpload").on("change", function (e) {
+        const selectedFile = e.target.files[0];
+        const fileName = selectedFile.name
+
+        const formData = new FormData();
+        formData.append("file", selectedFile);
+        if(uploadFile.getFileGroupId()) formData.append("fileGroupId", uploadFile.getFileGroupId());
+
+        Http.filePost("/api/uploadFile", formData).then((res) => {
+            if(res['httpStatus'] === 200) {
+                uploadFile.setFileGroupId(res.message.fileGroupId)
+                uploadFile.addFileList({'originalFileName': fileName, 'id': res.message.id})
+            }
+        })
+    })
+})
+
+ClassicEditor
+    .create( document.querySelector( '#content' ), {
+        extraPlugins: [MyCustomUploadAdapterPlugin]
+    } )
+    .then( editor => {
+        ckEditor = editor;
+    } )
+    .catch( error => {
+    } );
+
+function MyCustomUploadAdapterPlugin(editor) {
+    editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+        return new MyUploadAdapter(loader)
+    }
+}
