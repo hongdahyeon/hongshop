c4043b94537a3caa898643a229a86948627f5983
hongdahyeon <hyeon8287@gmail.com>
2023-12-29 09:38:42 +0900

    commit 200 : 사용자 비활성화 정보 수정

--- src/main/java/hongshop/hongshop/domain/userEnable/HongUserEnableRepository.java
+++ src/main/java/hongshop/hongshop/domain/userEnable/HongUserEnableRepository.java
@@ -4,6 +4,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+/**
+* @fileName HongUserEnableRepository
+* @author dahyeon
+* @version 1.0.0
+* @date 2023-12-29
+* @summary  (1) findByHongUserId: 사용자의 Id를 통해 비활성화 사유 정보 조회 (삭제여부 N)
+ *          (2) findByHongUserIdLong: 사용자의 uid를 통해 비활성화 정보 조회 (삭제여부 N)
+ *          -> 사용자를 통해 비활성화를 활성화로 변경할떄 삭제여부를 N에서 Y로 변경한다.
+**/
+
+
 public interface HongUserEnableRepository extends JpaRepository<HongUserEnable, Long> {
 
     @Query("SELECT m FROM HongUserEnable m WHERE m.hongUser.userId = :userId and m.deleteYn = 'N'")
